a3db9421e82ccace6168b043c8d8a749827fb49f
Alekhine161 <80931588+Alekhine161@users.noreply.github.com>
2021-03-22 19:00:07 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("*******Hello World trying webhook ****---");
+		System.out.println("*******Hello World trying webhook now ****---");
 	}
 }
